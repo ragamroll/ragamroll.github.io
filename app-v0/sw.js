ef0016dd825245b0a90bb447cd58051e7eff6399
ragamroll <ragamroll@gmail.com>
2026-07-16 22:12:21 -0700

    updated example
    swaravali was using c12. Changed it such that it can be played in any mela_01 to mela_72

--- app-v0/sw.js
+++ app-v0/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ragamroll-63ed345';
+const CACHE = 'ragamroll-64ea3cd';
 const ASSETS = [
   './', './index.html', './app.js', './version.js', './manifest.webmanifest',
   './vendor/preact.module.js', './vendor/hooks.module.js', './vendor/htm.module.js', './vendor/htm-preact.js',
--- app-v0/version.js
+++ app-v0/version.js
@@ -1,2 +1,2 @@
-export const VERSION = '63ed345';
+export const VERSION = '64ea3cd';
 export const BUILD_DATE = '2026-07-16';
